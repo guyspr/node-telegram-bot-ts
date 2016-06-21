51f1d071b98533c53aee35bc7e0213f40f5bb81a
Guy Spronck <guy.spronck@gmail.com>
2016-06-21 14:24:34 +0200

    Add typings and basic setup

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,8 @@
+/// <reference path="../typings/node.d.ts"/>
+var botgram = require('botgram');
+import { Config } from './config'; // Contains application configuration
 import { ICommand } from './ICommand';
-import { CommandList } from './CommandList';
+import { CommandList } from './CommandList'; // Contains all active commands
 
 /**
  * Base application class, used to run commands
@@ -20,3 +23,12 @@ class App {
 }
 
 let application = new App(CommandList);
+
+let bot = new botgram(Config.token);
+console.log("Bot is up and running!");
+
+// Catch-all for commands
+bot.command(function(msg, reply, next) {
+  console.log(msg)
+  application.run(msg.command, msg, reply);
+});
