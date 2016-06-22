be3ad90359fb5460c35871ee4d40c1fffd0d2081
Guy Spronck <guy.spronck@gmail.com>
2016-06-22 13:54:02 +0200

    Sort commands a-z

--- src/CommandList.ts
+++ src/CommandList.ts
@@ -4,18 +4,18 @@
 
 import { ICommand } from './Commands/ICommand';
 
-import { HelloWorld } from './Commands/HelloWorld';
-import { Echo } from './Commands/Echo';
-import { Forecast } from './Commands/Forecast';
-import { Joke } from './Commands/Joke';
 import { Doekoe } from './Commands/Doekoe';
+import { Echo } from './Commands/Echo';
 import { Emote } from './Commands/Emote';
+import { Forecast } from './Commands/Forecast';
+import { HelloWorld } from './Commands/HelloWorld';
+import { Joke } from './Commands/Joke';
 
 export let CommandList: ICommand[] = [
-  new HelloWorld(),
-  new Echo(),
-  new Forecast(),
-  new Joke(),
   new Doekoe(),
-  new Emote()
+  new Echo(),
+  new Emote(),
+  new Forecast(),
+  new HelloWorld(),
+  new Joke()
 ];
