49c19aab65954fd2d5b0f20b6b189c5119bc10d1
Guy Spronck <guy.spronck@gmail.com>
2016-06-22 12:13:55 +0200

    Add /joke command

--- src/CommandList.ts
+++ src/CommandList.ts
@@ -6,9 +6,11 @@ import { ICommand } from './Commands/ICommand';
 import { HelloWorld } from './Commands/HelloWorld';
 import { Echo } from './Commands/Echo';
 import { Forecast } from './Commands/Forecast';
+import { Joke } from './Commands/Joke';
 
 export let CommandList: ICommand[] = [
   new HelloWorld(),
   new Echo(),
-  new Forecast()
+  new Forecast(),
+  new Joke()
 ];
---
+++ src/Commands/Joke.ts
@@ -0,0 +1,31 @@
+import { ICommand } from './ICommand';
+import * as Request from 'request';
+
+// Format for the incoming jokes
+interface JokeObj{
+  title: string;
+  selftext?: string;
+}
+
+export class Joke implements ICommand {
+  command = /joke$/;
+  help = "Tells a joke from /r/jokes";
+  usage = "Usage: /joke";
+
+  // Implement the execute command
+  exec(msg, reply): void {
+    this.getJoke((result:string) => {
+      reply.markdown(result);
+    }); 
+  }
+
+  getJoke(callback: (result:string) => void): void{
+    var url = 'https://www.reddit.com/r/jokes.json';
+    Request(url, function(error, response, body){
+      var parsed = JSON.parse(body).data.children;
+      var rnd = Math.floor(Math.random() * (parsed.length - 1)) + 1;
+      var joke = <JokeObj>parsed[rnd].data;
+      callback(`*${joke.title}*\r\n${joke.selftext}`);
+    });
+  }
+}
